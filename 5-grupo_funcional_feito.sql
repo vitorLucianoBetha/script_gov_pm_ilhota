e8da1ed6aea9d1c36c161b10fae1bc0868437be8
vitorLucianoBetha <vitor.luciano@betha.com.br>
2025-01-27 15:53:41 -0300

    BTHSC-132869

--- 5-grupo_funcional_feito.sql
+++ 5-grupo_funcional_feito.sql
@@ -3,33 +3,32 @@ CALL bethadba.pg_setoption('wait_for_commit','on');
 CALL bethadba.pg_habilitartriggers('off');
 call bethadba.pg_setoption('fire_triggers','off');
 COMMIT;
+
 if  exists (select 1 from sys.sysprocedure where creator = (select user_id from sys.sysuserperms where user_name = current user) and proc_name = 'cnv_grupo_funcional') then
 	drop procedure cnv_grupo_funcional;
-end if
-;
+end if;
 
 create procedure tecbth_delivery.cnv_grupo_funcional()
 begin
 	ooLoop: for oo as cnv_grupo_funcional dynamic scroll cursor for
-		select 1 as w_i_entidades,CdFilial as w_i_grupos,upper(trim(NmFilial)) as w_nome 
-		from tecbth_delivery.gp001_filial as filial
+		select 1 as w_i_entidades,cdGrupoFuncional as w_i_grupos,DsGrupoFuncional as w_nome 
+		from tecbth_delivery.gp001_grupoFuncional
 		order by 1,2 asc
+		
 	do
 		message 'Ent.: '||w_i_entidades||' Gru.: '||w_i_grupos||' Nom.: '||w_nome to client;
 		
 		insert into bethadba.grupos(i_entidades,i_grupos,nome) 
 		values(w_i_entidades,w_i_grupos,w_nome);
 	end for;
-end
-;
+end;
 
-call cnv_grupo_funcional()
-;
 
-commit
-;
+
+commit;
 
 CALL bethadba.dbp_conn_gera(1, 2019, 300);
 CALL bethadba.pg_setoption('wait_for_commit','on');
 CALL bethadba.pg_habilitartriggers('off');
 COMMIT;
+
