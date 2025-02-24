4491bcda31aa97e21fc4118f0ac57c74f9ce15d0
andrefulsao <andre.nunes@betha.com.br>
2025-02-24 08:37:54 -0300

    ajuste ordem

--- 17-tipos_afastamento_feito.sql
+++ 17-tipos_afastamento_feito.sql
@@ -72,6 +72,15 @@ commit
 update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = (select max(ta.i_tipos_afast) + gp001_MOTIVOAFASTAMENTOax.i_tipos_afast from bethadba.tipos_afast ta)
 where afastamento_antes is not null or faltas_antes = 2;
 
+--- FALTAS JA ESTAVAM INSERIDAS NESTE CASO SOMENTE ATUALIZEI A TABELA AUXILIAR
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 4 where i_tipos_afast = 36 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 5 where i_tipos_afast = 37 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 7 where i_tipos_afast = 39 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 8 where i_tipos_afast = 40 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 9 where i_tipos_afast = 41 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 10 where i_tipos_afast = 42 and faltas_antes is not null;
+update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 11 where i_tipos_afast = 43 and faltas_antes is not null;
+
 --- INSERE EFETIVAMENTE OS AFASTAMENTOS DO CONCORRENTE PARA BETHA
 insert into bethadba.tipos_afast(i_tipos_afast,i_tipos_movpes,descricao,classif,perde_temposerv,busca_var,dias_prev) on existing skip
 select i_tipos_afast,
@@ -83,15 +92,6 @@ select i_tipos_afast,
 			dias_prev
 from tecbth_delivery.gp001_MOTIVOAFASTAMENTOax gm
 where gm.afastamento_antes is not null or faltas_antes = 2
-
---- FALTAS JA ESTAVAM INSERIDAS NESTE CASO SOMENTE ATUALIZEI A TABELA AUXILIAR
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 4 where i_tipos_afast = 36 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 5 where i_tipos_afast = 37 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 7 where i_tipos_afast = 39 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 8 where i_tipos_afast = 40 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 9 where i_tipos_afast = 41 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 10 where i_tipos_afast = 42 and faltas_antes is not null;
-update tecbth_delivery.gp001_MOTIVOAFASTAMENTOax set i_tipos_afast = 11 where i_tipos_afast = 43 and faltas_antes is not null;
 
 -- atualizando as classificações de acordo com o depara repassado pela implantação
 update bethadba.tipos_afast set classif = 2 where i_tipos_afast = 22;
