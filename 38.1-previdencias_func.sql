c5def5c79e12b28d5645b5fbfec35cff2bfa9128
andrefulsao <andre.nunes@betha.com.br>
2025-02-26 09:59:05 -0300

    ajuste

--- 38.1-previdencias_func.sql
+++ 38.1-previdencias_func.sql
@@ -5,6 +5,16 @@
 -- BTHSC-151481 Bug em Matrículas | Nível e Faixa salarial e Previdencia
 
 --select * from tecbth_delivery.gp001_TIPOFUNDOPREVIDENCIA gt 
+-- NECESSARIO VERIFICAR SE NÃO EXISTEM MAIS PLANOS DE PREVIDENCIA, NESTE CASO SÓ EXISTIA O 1, O 0 ERA PARA PREVIDENCIA FEDERAL
 
 insert into bethadba.hist_planos_previd_func(i_entidades,i_funcionarios,i_planos_previd,dt_inicial,dt_final) on existing skip
 select 1, gh.cdMatricula, 5, date(gh.dtInicio) as dataInicio, date(gh.dtFim)  as dataFim from tecbth_delivery.gp001_HISTORICOFUNDOPREVIDENCIA gh
+-- no caso de existir planos 0 adicionar (where cdFundoPrevidencia = 1) também rodar o update abaixo
+
+update bethadba.hist_funcionarios as f set f.fundo_prev = 'S' where exists(select first 1 from tecbth_delivery.gp001_HISTORICOFUNDOPREVIDENCIA a 
+where a.cdMatricula = f.i_funcionarios and a.cdFundoPrevidencia = 1 and f.dt_alteracoes between a.dtInicio and a.dtFim)
+
+/* somente quando há planos 0
+update bethadba.hist_funcionarios as f set f.prev_federal = 'S' where exists(select first 1 from tecbth_delivery.gp001_HISTORICOFUNDOPREVIDENCIA a 
+where a.cdMatricula = f.i_funcionarios and a.cdFundoPrevidencia = 0 and f.dt_alteracoes between a.dtInicio and a.dtFim)
+*/
